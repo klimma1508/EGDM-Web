57516897fc457dd32ec7e346c29795a54938efec
klimma1508 <martin.klims1@gmail.com>
2022-08-17 21:23:20 +0200

    added redirect to login if user isnt loged in or isnt saved in cookies added homepage render using ejs added login and logout added libraries: sync-mysql, body-parser, path, fs, cookie-parser, ejs added function to check user if is loged in or saved in cookies
    Next:
    Create page to add users
    Make passwords secure with "salt" and hash

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const https = require("https")
 
 const fs = require("fs")
 
+const path = require("path")
+
+var bodyParser = require('body-parser')
+
+const mysqlSync = require("sync-mysql")
+
+var conSync = new mysqlSync({
+  host: "192.168.0.100",
+  user: "root",
+  password: "Synology123.",
+  database: "EGDM"
+});
+
+const cookieParser = require("cookie-parser")
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const credentials = {
   key: fs.readFileSync("ssl/key.pem", "utf8"),
@@ -13,10 +29,175 @@ const credentials = {
 }
 
 
+app.use(express.static(path.join(__dirname, "..", "public")))
 app.set("view engine", "ejs")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use(
+  cookieParser({
+    httpOnly: false,  // Don't let browser javascript access cookies.
+    secure: false, // Only use cookies over https.
+  })
+)
+
+
+
+//Ceck user if is login
+function checkUser(req, res) {
+  if (req.cookies.user) {
+      const result = conSync.query("SELECT * FROM Accounts WHERE username = '" + req.cookies.user + "'")
+      try{
+              console.log(result)
+                           
+              return result[0].username == req.cookies.user
+          
+      }
+      catch{
+          return false
+      }
+     
+  } else {
+      return false
+  }
+}
+
+
+//routes
+
+app.get("/", (req, res) => {
+  var result = checkUser(req, res)
+  console.log("result:  " + result)
+  if(result){
+      res.locals.user = req.cookies.user
+      res.redirect("/home")
+  }
+  else{
+      res.redirect("/login")
+  }
+
+
+  
+})
+
+
+app.get("/login", (req, res) => {
+  var error = ""
+  return res.render("login",{
+      error
+  })
+})
+
+app.post("/login", urlencodedParser, (req, res) => {
+
+  var error = ""
+  let { user, password, remember } = req.body
+
+  console.log(user)
+  console.log(password)
+  console.log(remember? true : false)
+
+  if (user == "" && password == ""){
+      error = "username and password is required"
+      return res.render("login", {
+          error
+      })
+  }
+
+  if (user == ""){
+      error = "Username is required"
+      return res.render("login", {
+          error
+      })
+  }
+
+  if (password == ""){
+      error = "Password is required"
+      return res.render("login", {
+          error
+      })
+  }
+
+
+  try{
+      con.connect(function(err) {
+
+          var result = conSync.query("SELECT * FROM Accounts WHERE username = '" + user + "'")
+          
+            
+  
+
+  
+            try{
+                  if (result[0].password == password){
+                      res.locals.user = user
+                      if(remember){
+                          res.cookie(`user`,user,{
+                              secure: false,
+                              httpOnly: false,
+                              sameSite: 'lax'
+                          });
+                      }
+
+
+                      return res.render("home", {
+                          user
+                      })
+                  }
+                  else{
+          
+                      error = "Wrong username or password"
+                      return res.render("login", {
+                          error
+                      })
+                  }
+              }catch (err){
+                  error = "Wrong username or password"
+                  return res.render("login", {
+                      error
+                  })
+              }
+          
+        });
+  }catch(err){
+      error = "Wrong username or password"
+      return res.render("login", {
+          error
+      })
+
+  }
+
+})
+
+
+
+
+app.get("/home", (req, res) => {
+  var result = checkUser(req, res)
+  console.log("result:  " + result)
+  if(result){
+      res.locals.user = req.cookies.user
+      return res.render("home", {
+          user: res.locals.user,
+          svatek: "svatek",
+          dovolena: "dovolena",
+          prescas: "prescas"
+          })
+  }
+  else{
+      res.redirect("/login")
+  }
+
+
+  
+})
+
+
+app.get("/logout", (req, res) => {
+    
+  //delete cookies
+  res.clearCookie("user")
+  res.locals.username = ""
+  res.redirect("/")
 })
 
 
