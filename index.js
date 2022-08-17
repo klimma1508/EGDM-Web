12460f43a94ab4001f47eb2aa284abc6b27187dc
klimma1508 <martin.klims1@gmail.com>
2022-08-17 21:09:23 +0200

    Added HTTP and HTTPS, view engine set to ejs and added self signed ssl key

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
 
+const http = require("http")
+const https = require("https")
+
+const fs = require("fs")
+
+
+const credentials = {
+  key: fs.readFileSync("ssl/key.pem", "utf8"),
+  cert: fs.readFileSync("ssl/cert.pem", "utf8"),
+}
+
+
+app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
-})
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
